f8dd799bc371173d6277483de2f3c886fa1a38e7
OumaArera <johnouma999@gmail.com>
2025-02-05 13:26:20 +0300

    feat: patient manager

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 import Login from "./Login";
 import CareGiverDashboard from "./CareGiverDashboard";
 import SuperUserDashboard from "./SuperUserDashboard";
@@ -7,14 +8,29 @@ import ProtectedRoute from "./ProtectedRoute";
 import CreateUser from "./CreateUser";
 
 function App() {
+  const [userRole, setUserRole] = useState(null);
+
+  useEffect(() => {
+    const storedRole = localStorage.getItem("role");
+    if (!storedRole) {
+      setUserRole(null);
+    } else {
+      setUserRole(storedRole);
+    }
+  }, []);
+
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Login />} />
 
+        {/* Redirect to home (Login) if user reloads without a role */}
+        {!userRole && <Route path="*" element={<Navigate to="/" />} />}
+
         <Route element={<ProtectedRoute allowedRoles={["care giver"]} />}>
-          <Route path="/care giver" element={<CareGiverDashboard role="care giver" />} />
+          <Route path="/care-giver" element={<CareGiverDashboard role="care giver" />} />
         </Route>
+
         <Route element={<ProtectedRoute allowedRoles={["superuser"]} />}>
           <Route path="/createUser" element={<CreateUser role="superuser" />} />
         </Route>
