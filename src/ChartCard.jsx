7df82314ab8a5a868ebdf45b0a97d0c1c86ca96e
OumaArera <johnouma999@gmail.com>
2025-02-07 11:58:22 +0300

    feat: complete

--- src/ChartCard.jsx
+++ src/ChartCard.jsx
@@ -65,9 +65,9 @@ const ChartCard = ({ chart, onClose }) => {
           {/* <h2 className="text-xl font-bold mb-4 text-gray-900">
             Behavior Chart for {chart.patientName}
           </h2> */}
-          <div className="text-lg mb-4 font-bold  text-gray-900">
-            <p>Name: {chart.patientName}</p>
-            <p>Date: {formattedDate}</p>
+          <div className="text-sm mb-4 font-bold  text-gray-900">
+            <p>Patient Name: {chart.patientName}</p>
+            <p>Date Taken: {formattedDate}</p>
             <p>Care Giver: {chart.careGiver}</p>
           </div>
 
