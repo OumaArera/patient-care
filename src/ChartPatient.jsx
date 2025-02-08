8885d156282807c2504af22b8600932a5bb42fd3
OumaArera <johnouma999@gmail.com>
2025-02-08 21:46:44 +0300

    feat: complete

--- src/ChartPatient.jsx
+++ src/ChartPatient.jsx
@@ -112,7 +112,7 @@ const ChartPatient = () => {
 
                 {/* Overlay for NewCharts */}
                 {showNewCharts && charts.length > 0 && chartData.length > 0 && (
-                  <div className="absolute top-12 left-1/2 transform -translate-x-1/2 bg-gray-900 p-6 rounded-lg shadow-lg w-3/4 h-[80vh] overflow-y-auto z-50 border border-gray-700">
+                  <div className="absolute top-12 left-1/2 transform -translate-x-1/2 bg-gray-900 p-6 rounded-lg shadow-lg w-[80vh]h-[80vh] overflow-y-auto z-50 border border-gray-700">
                     <button
                         className="absolute top-2 right-2 text-white hover:text-gray-400"
                         onClick={() => setShowNewCharts(false)}
