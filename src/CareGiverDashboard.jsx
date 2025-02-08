80061774065c5715ec48359cbecfa59d3f4303bd
OumaArera <johnouma999@gmail.com>
2025-02-08 17:55:05 +0300

    feat: complete

--- src/CareGiverDashboard.jsx
+++ src/CareGiverDashboard.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import {
   FaChartBar, FaPills, FaNewspaper, FaUser, FaSignOutAlt, FaLock
