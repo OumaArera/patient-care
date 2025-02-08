c39f50807b00d756da63bfee2a82437ba15bdeec
OumaArera <johnouma999@gmail.com>
2025-02-08 11:31:54 +0300

    feat: complete

--- services/updateMedAdmin.js
+++ services/updateMedAdmin.js
@@ -1,6 +1,8 @@
 export const updateMedAdmin = async (medicationAdministrationId, updatedData) => {
     const token = localStorage.getItem("token");
     const url = "https://patient-care-server.onrender.com/api/v1/medication-administrations";
+    console.log("ID on Method:", medicationAdministrationId);
+        console.log("Status on Method:", updatedData);
     try {
         const response = await fetch(`${url}/${medicationAdministrationId}`, {
             method: "PUT",
