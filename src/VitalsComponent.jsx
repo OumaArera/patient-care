e1243d24980ec0df2f233a04a135fbf46fe835f9
OumaArera <johnouma999@gmail.com>
2025-02-14 11:43:39 +0300

    feat: complete

--- src/VitalsComponent.jsx
+++ src/VitalsComponent.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 const VitalsComponent = ({ vitalsData, handleVitalsChange }) => {
   const [errors, setErrors] = useState({});
-  const [vitals, setVitals] = useState(vitals);
+  const [vitals, setVitals] = useState(vitalsData);
 
   const validateVitals = () => {
     const newErrors = {};
