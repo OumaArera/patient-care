7143464a5c370c8ff5b414a6b56944ab6370d986
OumaArera <johnouma999@gmail.com>
2025-02-18 00:54:33 +0300

    feat: change message

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -66,7 +66,7 @@ const LandingPage = () => {
           animate={{ opacity: 1, y: 0 }}
           exit={{ opacity: 0, y: -20 }}
           transition={{ duration: 1 }}
-          className="relative z-10 bg-gray-100 text-black bg-opacity-75 p-6 rounded-lg shadow-lg max-w-lg text-center"
+          className="relative z-10 text-black bg-opacity-75 p-6 rounded-lg shadow-lg max-w-lg text-center"
         >
           <p className="text-3xl font-semibold">{content.message}</p>
         </motion.div>
