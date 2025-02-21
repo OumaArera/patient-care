29df63527395100057262aee76ed1be55653a102
OumaArera <johnouma999@gmail.com>
2025-02-21 16:29:36 +0300

    feat: complete

--- services/getAppointments.js
+++ services/getAppointments.js
@@ -3,7 +3,7 @@ export const getAppointments = async (patient) => {
     try {
         const token = localStorage.getItem("token");
         const response = await fetch(
-        `https://patient-care-server.onrender.com/api/v1/appointments?patient=${patient}&pageSize=366&pageNumber=1`,
+        `https://patient-care-server.onrender.com/api/v1/appointments?patient=${patient}`,
         {
             headers: { Authorization: `Bearer ${token}` },
         }
