3354bea0b369136c1657d785a9f003a9828b684c
OumaArera <johnouma999@gmail.com>
2025-03-01 16:11:08 +0300

    feat: complete

--- src/ChartPatient.jsx
+++ src/ChartPatient.jsx
@@ -121,12 +121,8 @@ const ChartPatient = () => {
 
                 {/* Overlay for NewCharts */}
                 {showNewCharts && selectedPatient && selectedPatientId === patient.patientId && chartData.length > 0 && (
-                  // <div 
-                  //   className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50"
-                  // >
-                  //   <div className="bg-gray-900 p-6 rounded-lg shadow-lg w-[80vw] h-[80vh] overflow-y-auto border border-gray-700 relative">
                   <div
-                    className="fixed inset-0 bg-opacity-50 flex justify-center items-center"
+                    className="fixed inset-0 bg-opacity-50 flex justify-center items-center z-50"
                     onClick={closChartModal}
                 >
                     <div
@@ -137,7 +133,7 @@ const ChartPatient = () => {
                       <NewCharts charts={selectedPatient} chartsData={chartData} />
                       <button
                         className="absolute top-2 right-2 text-white hover:text-gray-400"
-                        onClick={() => closChartModal}
+                        onClick={closChartModal}
                       >
                         ✖
                       </button>
