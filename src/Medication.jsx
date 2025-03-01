168c28e1a80fac4003b1ddf84a8f6312a482f80b
OumaArera <johnouma999@gmail.com>
2025-03-01 05:02:53 +0300

    feat: complete

--- src/Medication.jsx
+++ src/Medication.jsx
@@ -163,11 +163,13 @@ const Medication = () => {
                                 required
                             >
                                 <option value="">Select a Resident</option>
-                                {patients.map((patient) => (
-                                    <option key={patient.patientId} value={patient.patientId}>
-                                        {patient.firstName} {patient.lastName}
-                                    </option>
-                                ))}
+                                {[...patients]
+                                    .sort((a, b) => `${a.firstName} ${a.lastName}`.localeCompare(`${b.firstName} ${b.lastName}`))
+                                    .map((p) => (
+                                        <option key={p.patientId} value={p.patientId}>
+                                            {p.firstName} {p.lastName}
+                                        </option>
+                                    ))}
                             </select>
                         )}
                     </div>
