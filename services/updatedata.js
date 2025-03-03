06a3162a1d6cc0666a989147754175f786f8d803
OumaArera <johnouma999@gmail.com>
2025-03-03 09:49:16 +0300

    feat: complete

--- services/updatedata.js
+++ services/updatedata.js
@@ -1,5 +1,7 @@
 export const updateData = async (url, updatedData) => {
     const token = localStorage.getItem("token");
+    console.log("URL: ", url);
+    console.log("Update Data: ", updatedData);
     try {
         const response = await fetch(url, {
             method: "PUT",
