b4c6174444a6796e859236f604ba9e927adfabef
OumaArera <johnouma999@gmail.com>
2025-03-05 16:03:10 +0300

    feat: Branch details

--- src/AllCharts.jsx
+++ src/AllCharts.jsx
@@ -187,7 +187,7 @@ const AllCharts = () => {
                                                     acc[behavior.category].push(existingRow);
                                                 }
                                                 const chartDate = new Date(chart.dateTaken);
-                                                chartDate.setDate(chartDate.getDate() - 1); // Reduce the date by 1 day
+                                                chartDate.setDate(chartDate.getDate()); 
 
                                                 existingRow.days[chartDate.getDate() - 1] = behavior.status === "Yes" ? "✔️" : "❌";
                                                 // existingRow.days[new Date(chart.dateTaken).getDate() - 1] = behavior.status === "Yes" ? "✔️" : "❌";
