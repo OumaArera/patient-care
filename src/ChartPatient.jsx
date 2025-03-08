da4eede58fc154adea0e242f80760bfb3429bee0
OumaArera <johnouma999@gmail.com>
2025-03-09 00:57:44 +0300

    feat: late submissions

--- src/ChartPatient.jsx
+++ src/ChartPatient.jsx
@@ -159,7 +159,7 @@ const ChartPatient = () => {
                   </div>
                 </div>
               )}
-              {showCharts && selectedPatientId &&(
+              {showCharts&& selectedPatient && selectedPatientId === patient.patientId && selectedPatientId &&(
                 <div
                   className="fixed inset-0 bg-opacity-50 flex justify-center items-center z-50"
                   onClick={closePendingCharts}
