ac731eca37d98dae3b44e2408ab093b7cf276c0a
OumaArera <johnouma999@gmail.com>
2025-03-11 15:19:34 +0300

    feat: late submissions

--- src/ChartVitals.jsx
+++ src/ChartVitals.jsx
@@ -252,7 +252,7 @@ const ChartVitals = () => {
                                         dateFormat="yyyy-MM-dd HH:mm:ss"
                                         minTime={dayjs().set("hour", 8).set("minute", 0).toDate()}
                                         maxTime={dayjs().set("hour", 9).set("minute", 59).toDate()}
-                                        filterDate={isValidDate}
+                                        // filterDate={isValidDate}
                                         className="p-2 bg-gray-800 text-white border border-gray-700 rounded w-full"
                                     />
                                 </div>
